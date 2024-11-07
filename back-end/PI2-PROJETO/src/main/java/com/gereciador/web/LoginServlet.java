a239f677a23ad65f8751e61975df7d52776aab72
Marcelo Afonso Coelho <133604815+MarceloAfonso-dev@users.noreply.github.com>
2024-11-07 19:38:40 -0300

    Update LoginServlet.java

--- back-end/PI2-PROJETO/src/main/java/com/gereciador/web/LoginServlet.java
+++ back-end/PI2-PROJETO/src/main/java/com/gereciador/web/LoginServlet.java
@@ -3,6 +3,7 @@ package com.gereciador.web;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,39 +20,28 @@ public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        // Obtém os parâmetros do formulário de login
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String login = request.getParameter("login");
         String senha = request.getParameter("senha");
 
         Connection connection = null;
         try {
-            // Cria uma conexão com o banco de dados
             connection = new ConnectionFactory().getConnection();
-            
-            // Instancia o UsuarioDAO com a conexão
             UsuarioDAO usuarioDAO = new UsuarioDAO(connection);
-
-            // Verifica se o usuário existe no banco de dados
             Usuario usuario = usuarioDAO.buscarPorEmailSenha(login, senha);
 
             if (usuario != null) {
-                // Login bem-sucedido, cria uma sessão para o usuário
                 HttpSession session = request.getSession();
                 session.setAttribute("usuarioLogado", usuario);
-                
-                // Redireciona para a dashboard (um arquivo .html)
+                session.setAttribute("loginTime", System.currentTimeMillis());
                 response.sendRedirect(request.getContextPath() + "/dashboard");
             } else {
-                // Falha no login, redireciona de volta para o login com erro
                 response.sendRedirect("index.html?error=true");
             }
 
         } catch (SQLException e) {
             throw new ServletException("Erro ao conectar ao banco de dados", e);
         } finally {
-            // Fecha a conexão com o banco de dados
             if (connection != null) {
                 try {
                     connection.close();
